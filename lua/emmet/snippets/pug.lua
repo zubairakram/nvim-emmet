60c4b0dd0500eacd47cdd68641e6237be928bb21
Zubair Akram <zubair616@gmail.com>
2022-02-21 23:07:46 +0500

    snippets added

---
+++ lua/emmet/snippets/pug.lua
@@ -0,0 +1,5 @@
+local pug = {}
+
+pug["!!!"] = "{doctype html}"
+
+return pug
---
+++ lua/emmet/snippets/variables.lua
@@ -0,0 +1,10 @@
+local variables = {}
+
+variables["lang"] = "en"
+variables["locale"] = "en-US"
+variables["charset"] = "UTF-8"
+variables["indentation"] = "\t"
+variables["newline"] = "\n"
+
+return variables
+

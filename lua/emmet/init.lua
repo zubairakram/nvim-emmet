dc489de0fae09784d3beb446cd1504e44f1d50cf
Zubair Akram <zubair616@gmail.com>
2022-10-15 20:41:38 +0500

    cleaner datasturcture syntax

--- lua/emmet/init.lua
+++ lua/emmet/init.lua
@@ -1,1 +1,10 @@
-print("hello lua plugin")
+local html = require("snippets.html")
+local css = require("snippets.css")
+
+
+local function expand_tag(tag)
+    return string.format("<%s></%s>", tag, tostring(tag))
+end
+
+
+print(expand_tag("h1"))

b52c44b8c0dd0420cde2fc28828f8641f7c46e01
Zubair Akram <zubair616@gmail.com>
2023-03-27 23:10:16 +0500

    configuration and .editorconfig

--- lua/emmet/markup/lorem/init.lua
+++ lua/emmet/markup/lorem/init.lua
@@ -1,3 +1,10 @@
-local latin = require "latin"
+-- check speling in comments
+local russian = require('russian')
+local latin = require('latin')
+local spanish = require('spanish')
 
-local russian = require "russian"
+local function hello()
+    for i, x in pairs(table) do
+        print('hello world')
+    end
+end
